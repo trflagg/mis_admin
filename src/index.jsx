eb9cb40bc2db643bbb078ba81afd82dd8579f806
Taylor Flagg <trflagg@gmail.com>
2017-03-30 18:31:24 -0400

    Add Redux to request profiles from server when page loads
    Doesn't yet connect to store or components.

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 import App from './App';
+import { getProfiles } from './actions';
+import rootReducer from './reducers';
 import './index.css';
 
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+
+store.dispatch(getProfiles());
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root'),
 );
